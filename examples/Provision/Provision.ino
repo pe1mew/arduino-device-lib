1111ce08239f5882e372a163fb545a6eccff617a
Remko <pe1mew@gmail.com>
2019-10-05 10:54:25 +0200

    Added example files using commissioning. These examples use the feature of the RN2483 to store credentials in local EEPROM.

---
+++ examples/Provision/Provision.ino
@@ -0,0 +1,36 @@
+#include <TheThingsNetwork.h>
+
+// Set your AppEUI and AppKey
+const char *appEui = "0000000000000000";
+const char *appKey = "00000000000000000000000000000000";
+
+#define loraSerial Serial1
+#define debugSerial Serial
+
+// Replace REPLACE_ME with TTN_FP_EU868 or TTN_FP_US915
+#define freqPlan REPLACE_ME
+
+TheThingsNetwork ttn(loraSerial, debugSerial, freqPlan);
+
+void setup()
+{
+  loraSerial.begin(57600);
+  debugSerial.begin(9600);
+
+  // Wait a maximum of 10s for Serial Monitor
+  while (!debugSerial && millis() < 10000)
+    ;
+
+  debugSerial.println("-- STATUS");
+  ttn.showStatus();
+
+  debugSerial.println("-- PROVISION RN2483");
+  ttn.provision(appEui, appKey); //  saveState() is invoked automatically
+}
+
+void loop()
+{
+  debugSerial.println("-- RN2483 PROVISIONED");
+
+  for (;;);
+}
